0ff75c04d4cbb4f1002f5c726269bcb3639f328f
James Park <james.park@meraki.net>
2018-04-16 12:58:07 -0700

    become seller flow: start forms for payment and return address
    This adds form UI for payment and return address submission. Form submission is
    done by clicking the next button (UX borrowed from Airbnb's flow).
    
    Submission posts to a working placeholder endpoint and for now the only
    requirement for submit success is that the endpoint returns a non-empty object.

--- js/BecomeSellerFlow.js
+++ js/BecomeSellerFlow.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import moment from "moment";
+import _ from "underscore";
 import {
   StyleSheet,
   ScrollView,
@@ -9,6 +11,7 @@ import {
   Text,
   Icon,
 } from 'react-native-elements';
+import t from 'tcomb-form-native';
 import Button from './shared/Button';
 import NavTabHeader from './shared/NavTabHeader';
 import FlowModal from './shared/FlowModal';
@@ -69,22 +72,180 @@ class Screen2 extends Component {
   }
 }
 
+const accountType = t.enums({
+  debit: "debit",
+  wechat: "wechat",
+  alipay: "alipay",
+}, "accountType");
+
+const months = _.reduce(moment.localeData().months(), (months, monthName, idx) => {
+  months[idx + 1] = monthName;
+  return months;
+}, {});
+
+const minYear = moment().year();
+
+const years = _.reduce(_.range(minYear, minYear + 20), (years, year) => {
+  years[year] = `${year}`;
+  return years;
+}, {})
+
+const debitDetails = t.struct({
+  type: accountType,
+  number: t.Number,
+  expMonth: t.enums(months),
+  expYear: t.enums(years),
+  cvc: t.Number,
+});
+
+const miscDetails = t.struct({
+  type: accountType,
+  name: t.String,
+  password: t.String,
+})
+
 class Screen3 extends Component {
   static displayName = "Screen3"
 
+  constructor() {
+    super();
+
+    const initValue = {};
+    this.state = {
+      value: initValue,
+      type: this.getType(initValue),
+    };
+  }
+
+  getType = value => {
+    if (value.type === "debit") {
+      return debitDetails;
+    } else {
+      return miscDetails;
+    }
+  }
+
+  onChange = value => {
+    const type = value.type !== this.state.value.type
+      ? this.getType(value)
+      : this.state.type;
+    this.setState({ value, type });
+  }
+
+  setForm = component => {
+    this.form = component;
+  }
+
+  onSubmit = () => {
+    const value = this.form.getValue();
+    if (value) {
+      // placeholder url for our endpoint
+      fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: "POST",
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(value),
+      }).then(response => response.json())
+      .then(responseJson => {
+        // expect any response for testing
+        if (!_.isEmpty(responseJson)) {
+          this.props.navigateNext();
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    }
+  }
+
   render() {
     return (
-      <View style={{ flex: 1 }}><Text>3</Text></View>
+      <ScrollView style={{ flex: 1 }}>
+        <Text h3>Payout Information</Text>
+        <Text>Enter your information correctly if you want to get paid!</Text>
+        <t.form.Form
+          ref={ this.setForm }
+          type={ this.state.type }
+          value={ this.state.value }
+          onChange={ this.onChange }
+        />
+        { this.props.prevButton }
+        <Button
+          key="nextButton"
+          onPress={ this.onSubmit }
+          title="Next"
+        />
+      </ScrollView>
     );
   }
 }
 
+const returnAddress = t.struct({
+  street: t.String,
+  street2: t.String,
+  city: t.String,
+  state: t.String,
+  zip: t.Number,
+});
+
 class Screen4 extends Component {
   static displayName = "Screen4"
 
+  constructor() {
+    super();
+
+    const initValue = {};
+    this.state = {
+      value: initValue,
+    };
+  }
+
+  setForm = component => {
+    this.form = component;
+  }
+
+  onSubmit = () => {
+    const value = this.form.getValue();
+    if (value) {
+      // placeholder url for our endpoint
+      fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: "POST",
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(value),
+      }).then(response => response.json())
+      .then(responseJson => {
+        // expect any response for testing
+        if (!_.isEmpty(responseJson)) {
+          this.props.navigateNext();
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    }
+  }
+
   render() {
     return (
-      <View style={{ flex: 1 }}><Text>4</Text></View>
+      <View style={{ flex: 1 }}>
+        <Text h3>Return Address</Text>
+        <Text>When cancellations, returns, or other issues arise we will ship your shoe back to this address.</Text>
+        <t.form.Form
+          ref={ this.setForm }
+          type={ returnAddress }
+        />
+        { this.props.prevButton }
+        <Button
+          key="nextButton"
+          onPress={ this.onSubmit }
+          title="Next"
+        />
+      </View>
     );
   }
 }
